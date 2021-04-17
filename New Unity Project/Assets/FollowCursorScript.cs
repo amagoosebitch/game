625c835991526f3a1fb664cb9e228c5f6156cfd1
Matvey Ilichev <matvey.ilichev@gmail.com>
2021-04-17 17:07:58 +0500

    wasd contol

---
+++ New Unity Project/Assets/FollowCursorScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursorScript : MonoBehaviour
+{
+    private Vector3 mousePosition;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        RotateToMousePosition();
+    }
+
+    void RotateToMousePosition()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var difference = mousePosition - transform.position; 
+        difference.Normalize();
+        transform.rotation = Quaternion.Euler(
+            0f, 0f, Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg);  
+    }
+}
