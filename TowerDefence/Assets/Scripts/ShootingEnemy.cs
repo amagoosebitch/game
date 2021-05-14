7068dc153c3c4755b6556851398914cde78429c8
amagoose <amagooseoj@mail.ru>
2021-05-14 12:12:52 +0500

    update ui and more

--- TowerDefence/Assets/Scripts/ShootingEnemy.cs
+++ TowerDefence/Assets/Scripts/ShootingEnemy.cs
@@ -7,6 +7,10 @@ public class ShootingEnemy : MonoBehaviour
 {
     private GameObject player;
     public GameObject Vector;
+    private bool flag = true;
+    private Coroutine fire;
+    public Transform firePoint;
+    
     void Start()
     {
         player = GameObject.Find("player");
@@ -15,24 +19,43 @@ public class ShootingEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateFace();
         var destination = player.transform.position - gameObject.transform.position;
         var distance = Math.Sqrt(Math.Pow(destination.x, 2) + Math.Pow(destination.y, 2));
-        if (distance <= 70 + double.Epsilon)
+        if (flag && distance <= 20)
         {
-            StartCoroutine(Shoot(destination));
+            flag = false;
+            fire = StartCoroutine(Shoot());
+        }
+        
+        if(fire != null && distance > 20 + double.Epsilon)
+        {
+            flag = true;
+            StopCoroutine(fire);
         }
     }
     
     
-    IEnumerator Shoot(Vector3 destination)
+    IEnumerator Shoot()
     {
         while (true)
         {
+            var destination = gameObject.transform.right;
             destination.Normalize();
-            var angle = Mathf.Atan2(destination.y, destination.x);
-            var rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
-            Instantiate(Vector, gameObject.transform.position, gameObject.transform.rotation);
-            yield return new WaitForSeconds(0.7f);
+            Instantiate(Vector, firePoint.position, firePoint.rotation);
+            yield return new WaitForSeconds(1f);
         }
     }
+    
+    void RotateFace()
+    {
+        var playerPosition = player.transform.position;
+        var difference = playerPosition - transform.position; 
+        difference.Normalize();
+        var angle =
+            Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(
+            0f, 0f, angle);  
+    }
+    
 }
--- TowerDefence/Assets/Scripts/VectorBehaviour.cs
+++ TowerDefence/Assets/Scripts/VectorBehaviour.cs
@@ -11,12 +11,8 @@ public class VectorBehaviour : MonoBehaviour
     private GameObject player;
     void Start()
     {
-        player = GameObject.Find("player");
-        var destination = player.transform.position - gameObject.transform.position;
-        var v2 = new Vector2(destination.x, destination.y);
-        v2.Normalize();
         if (rb.name != "AlgemEnemy")
-            rb.velocity = destination * speed;
+            rb.velocity = transform.right * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
