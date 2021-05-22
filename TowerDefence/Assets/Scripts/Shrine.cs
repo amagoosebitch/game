35e5d6ac0bcb0f0a59b8cef99a22290e0ed34418
amagoose <amagooseoj@mail.ru>
2021-05-23 03:39:26 +0500

    exit

--- TowerDefence/Assets/Scripts/Shrine.cs
+++ TowerDefence/Assets/Scripts/Shrine.cs
@@ -8,6 +8,15 @@ public class Shrine : MonoBehaviour
     // Start is called before the first frame update
     private int health = 1000;
 
+    [SerializeField] private Text hp;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp.text = health + " / " + "1000";
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
--- TowerDefence/Assets/Tests/ShrineTests.cs
+++ TowerDefence/Assets/Tests/ShrineTests.cs
@@ -8,22 +8,6 @@ using UnityEngine.TestTools;
 
 public class NewTestScript
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void ShrineTestSimplePasses()
-    {
-        var go = new GameObject();
-        var gameObject = MonoBehaviour.Instantiate(go);
-        var shrine = gameObject.AddComponent<Shrine>();
-        shrine.TakeDamage(100);
-        Type type = shrine.GetType();
-        FieldInfo fieldInfo = type.GetField("health", BindingFlags.Instance | BindingFlags.NonPublic);
-        var health = fieldInfo.GetValue(shrine);
-        Assert.That((int)health == 900);
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
     public IEnumerator ShrineTestWithEnumeratorPasses()
     {
