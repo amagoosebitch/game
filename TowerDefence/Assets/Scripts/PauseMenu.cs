c39a8a6fe9c04b4e1bcbdd9ec835a0075b4ba0dc
MatveyMatveyIl <matvey.ilichev@gmail.com>
2021-05-26 21:00:04 +0500

    new trainig scene

--- TowerDefence/Assets/Scripts/PauseMenu.cs
+++ TowerDefence/Assets/Scripts/PauseMenu.cs
@@ -5,33 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    private static bool isPause = false;
-    public GameObject pauseMenuUI;
-    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("Menu");
         }
-    }
-
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1;
-        isPause = false;
-    }
-
-    void PauseToMenu()
-    {
-        pauseMenuUI.SetActive(true);
-        isPause = true;
-    }
-
-    public void LoadMenu()
-    {
-        Time.timeScale = 1;
-        SceneManager.LoadScene("Menu");
     }
 }
