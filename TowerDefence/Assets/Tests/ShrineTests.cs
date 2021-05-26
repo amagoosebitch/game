bf278c8e30f99ff1b22ed84ea3a62b598607a448
MatveyMatveyIl <matvey.ilichev@gmail.com>
2021-05-26 09:56:00 +0500

    shooting test

--- TowerDefence/Assets/Tests/ShrineTests.cs
+++ TowerDefence/Assets/Tests/ShrineTests.cs
@@ -18,4 +18,37 @@ public class NewTestScript
         yield return null;
         Assert.That(gameObject == null);
     }
+    
+    [UnityTest]
+    public IEnumerator WallCreaterTest()
+    {
+        var go = new GameObject();
+        var gameObject = MonoBehaviour.Instantiate(go);
+        var wall = gameObject.AddComponent<wallCreater>();
+        wall.CreateWall();
+        Assert.That(wall.added);
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator ReloadBulletsTest()
+    {
+        var go = new GameObject();
+        var gameObject = MonoBehaviour.Instantiate(go);
+        var bullets = gameObject.AddComponent<Shooter>();
+        bullets.Recharge();
+        yield return new WaitForSeconds(2);
+        Assert.That(bullets.shootingFlag);
+    }
+    
+    [UnityTest]
+    public IEnumerator ShootBulletsTest()
+    {
+        var go = new GameObject();
+        var gameObject = MonoBehaviour.Instantiate(go);
+        var bullets = gameObject.AddComponent<Shooter>();
+        bullets.Shoot();
+        yield return new WaitForSeconds(1);
+        Assert.Greater(200, bullets.currentBullets);
+    }
 }
